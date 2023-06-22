0d51deea8d223a0c54282967b91569c94679c539
Justin Harper <jstnhrpr321@gmail.com>
2023-06-22 11:25:45 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Profile info div
+const overview = document.querySelector(".overview");
+const username = "theCS19";
+
+const apiFetch = async function () {
+    const userInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    console.log(data);
+    displayInfo(data);
+};
+
+apiFetch();
+
+const displayInfo = function(data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+    `;
+    overview.append(div);
+};
